b9a43b0076253aecbc42fd598034f7a2705bd796
sandyml <sandramayla@gmail.com>
2023-07-04 14:30:53 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import ToDoList from './ToDoList';
+import { Counter } from './Interview';
 
 function App() {
   return (
     <div className="App">
       <ToDoList/>
+      <Counter />
     </div>
   );
 }
---
+++ src/Interview.jsx
@@ -0,0 +1,38 @@
+// three buttons 
+// increment by one every one second 
+// reset button 
+// increment by one inderement by one
+
+import React, { useEffect, useState } from 'react';
+
+export const Counter = () => {
+
+ const[count, setCount] = useState(0);
+
+ useEffect(() => {
+  const timer = setInterval(() => 
+  setCount((prevCount) => prevCount + 1))
+  return () => clearInterval(timer)
+ }, [])
+
+ const timeIncrement = () => {
+  setCount(count + 1)
+ }
+ const timeDecrement = () => {
+  setCount(count - 1)
+ }
+ const timeReset = () => {
+  setCount(0)
+ }
+
+ return (
+  <div>
+   <h1>Counter: {count}</h1>
+   <div>
+    <button onClick={timeIncrement}>increment</button>
+    <button onClick={timeReset}>reset</button>
+    <button onClick={timeDecrement}>decrement</button>
+   </div>
+  </div> 
+ )
+}
